92cb8be718dba8e2771496dcc1efe8dcf6d15358
Arienangel <arienangelts@gmail.com>
2025-08-29 20:37:12 +0800

    add plurk response

--- plurk.py
+++ plurk.py
@@ -1,6 +1,7 @@
 import asyncio
 import inspect
 import logging
+from time import sleep
 
 import aiohttp
 import base36
@@ -8,8 +9,17 @@ import dateutil.parser
 
 logger = logging.getLogger('Plurk')
 
+class convert:
+    def to_url(id:int):
+        return f"https://www.plurk.com/p/{base36.dumps(int(id))}"
 
-async def get_search(query: str, n: int = 30, **kwargs) -> list[dict]:
+    def to_id(url:str):
+        if '/' in url:
+            return base36.loads(url.split('https://www.plurk.com/p/')[-1])
+        else:
+            return base36.loads(url)
+
+async def get_search(query: str, n: int = 30, wait: float = 0, **kwargs) -> list[dict]:
     """
     Search plurk post with query
 
@@ -22,15 +32,19 @@ async def get_search(query: str, n: int = 30, **kwargs) -> list[dict]:
     search_url = 'https://www.plurk.com/Search/search2'
     post_body = {"query": query}
     posts = list()
+    last_id = 0
     logger.info(f'Get Plurk search: {query}')
     async with aiohttp.ClientSession() as session:
         while len(posts) < n:
             try:
+                if wait > 0: sleep(wait)
                 async with session.post(search_url, data=post_body) as response:
                     if response.content_type != 'application/json':
                         break
                     body = await response.json()
                     users = body['users']
+                    if last_id == body['plurks'][-1]['id']: break
+                    else: last_id = body['plurks'][-1]['id']
                     for post in body['plurks']:
                         author = users[str(post['user_id'])]['display_name']
                         time = int(dateutil.parser.parse(post['posted']).timestamp())
@@ -43,3 +57,51 @@ async def get_search(query: str, n: int = 30, **kwargs) -> list[dict]:
                 logger.warning(f'{__name__}@{inspect.stack()[0][3]}(query={query}): {type(E).__name__}: {E}')
                 break
     return posts[:n][::-1]
+
+
+async def get_response(plurk_id, n: int = 400, wait: float = 0):
+    if isinstance(plurk_id, str): 
+        try:
+            plurk_id=int(plurk_id)
+        except:
+            plurk_id=convert.to_id(plurk_id)
+    search_url = 'https://www.plurk.com/Responses/get'
+    post_body = {"plurk_id": plurk_id, "from_response_id": 0}
+    responses = list()
+    last_id = 0
+    async with aiohttp.ClientSession() as session:
+        while len(responses) < n:
+            try:
+                if wait > 0: sleep(wait)
+                async with session.post(search_url, data=post_body) as response:
+                    if response.content_type != 'application/json':
+                        break
+                    body = await response.json()
+                    users = body['users']
+                    if len(body['responses'])==0: break
+                    if last_id == body['responses'][-1]['id']: break
+                    else: last_id = body['responses'][-1]['id']
+                    if len(users):
+                        for res in body['responses']:
+                            try:
+                                author = users[str(res['user_id'])]['display_name']
+                                time = int(dateutil.parser.parse(res['posted']).timestamp())
+                                content = res['content_raw']
+                                id = res['id']
+                                responses.append({'author': author, 'time': time, 'content': content, 'id': id})
+                            except:
+                                continue
+                    else:
+                        for res in body['responses']:
+                            try:
+                                author = res['handle']
+                                time = int(dateutil.parser.parse(res['posted']).timestamp())
+                                content = res['content_raw']
+                                id = res['id']
+                                responses.append({'author': author, 'time': time, 'content': content, 'id': id})
+                            except:
+                                continue
+                    post_body.update({'from_response_id': responses[-1]['id']})
+            except aiohttp.web.HTTPException as E:
+                break
+    return responses[:n]
